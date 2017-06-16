d191cc3f0ffc006efa4a75b391457739b1b4c912
itachi1706 <kennethsohyq@gmail.com>
2017-06-16 23:20:38 +0800

    Need to update the array checks

--- server.js
+++ server.js
@@ -29,16 +29,16 @@ bot.onText(/\/gadmin_add (.+)/, (msg, match) => {
     console.log('Splitting String ' + oldstring);
     match = match[1].split(" ");
     // Not enough length
-    if (match.length < 3) {
+    if (match.length < 2) {
         bot.sendMessage(msg.chat.id, "Not enough arguments to register a new answer!\n" +
             "You need to have a answer and a fake answer at least!\n\nFormat: /gadmin_add answer fake_answer [answer_type]");
         return;
-    } else if (match.length === 3) type = defType;
-    else type = match[3];
+    } else if (match.length === 2) type = defType;
+    else type = match[2];
     let message = "";
-    switch (database.createAnswer(dbConnection, match[1], match[2], type)) {
+    switch (database.createAnswer(dbConnection, match[0], match[1], type)) {
         case 0:
-            message = "Answer Created!\n\nReal Answer: " + match[1] + "\nFake Answer: " + match[2] + "\nAnswer Category: " + type;
+            message = "Answer Created!\n\nReal Answer: " + match[0] + "\nFake Answer: " + match[1] + "\nAnswer Category: " + type;
             break;
         case -1: message = "Unable to add category (" + type + "). Failed to add new answer"; break;
         case -2: message = "Unable to retrieve category (" + type + "). Failed to add new answer"; break;
