bc1de8d83a31c18e8ce2de0932df0dce013b8b34
itachi1706 <kennethsohyq@gmail.com>
2017-06-17 15:14:10 +0800

    Add users to the various databases on sending a message

--- database.js
+++ database.js
@@ -5,25 +5,60 @@
  * @param fake_answer Fake Ans
  * @param type Question Type
  * @return number 0 for success, -1 for fail to add category, -2 for fail to get category (not exists),
- * -3 for fail to add qn, -4 DB Unspecified Error
+ * -3 for fail to add qn
  */
 module.exports.createAnswer = function (db, answer, fake_answer, type) {
     type = type.toLowerCase();
-    db.getConnection((err, conn) => {
-        conn.query("INSERT INTO questions_category SET ?", {name: type}, (error, results, fields) => {
-            if (error) return -1;
-        });
-        let queryid = -1;
-        conn.query("SELECT id from questions_category WHERE name = ?", [type], (error, results, fields) => {
-            if (error) return -2;
-            queryid = results.insertId;
-        });
-        conn.query("INSERT INTO questions SET ?", {answer: answer, fake: fake_answer, category: queryid}, (error, results, fields) => {
-            if (error) return -3;
-        });
-        conn.release();
-        if (err) return -4;
-        return 0;
+    db.query("INSERT IGNORE INTO questions_category SET ?", {name: type}, (error, results, fields) => {
+        if (error) return -1;
     });
-    return -4;
+    db.query("SELECT id from questions_category WHERE name = ?", [type], (error, results, fields) => {
+        if (error) return -2;
+        let queryid = results[0].id;
+        db.query("INSERT IGNORE INTO questions SET ?", {answer: answer, fake: fake_answer, category: queryid},
+            (error, results, fields) => {
+                if (error) return -3;
+            });
+    });
+    return 0;
+};
+
+/**
+ * Add a user if they do not exist
+ * @param db Database Object
+ * @param msg Telegram Message Object
+ * @return boolean true if success, false otherwise
+ */
+module.exports.addUser = function (db, msg) {
+    if (msg.chat.type === "channel") return false;
+    if (msg.chat.type === "private") {
+        // Personal Chat, add to chat
+        db.query("INSERT IGNORE INTO players_private SET ?", {player_id: msg.from.id, chat_id: msg.chat.id}, (error, res, f) => {
+            if (error) {
+                console.log("An error occurred adding a user private chat. Error: " + error.stack);
+                console.log("User: " + msg.from.id + " | Chat: " + msg.chat.chatId);
+                return false;
+            }
+            return true;
+        });
+    }
+    // Group or Supergroup
+    let name = msg.from.first_name;
+    if (typeof msg.from.last_name !== 'undefined') {
+        name += msg.from.last_name;
+    }
+    db.query("INSERT IGNORE INTO players SET ?", {player_id: msg.from.id, player_name: name, chat_id: msg.chat.id,
+    chat_type: msg.chat.type, chat_title: msg.chat.title}, (err, r, f) => {
+        if (err) {
+            console.log("An error occurred adding a new user. Error: " + err.stack);
+            console.log("User: " + msg.from.id + " | Chat: " + msg.chat.chatId);
+            return false;
+        }
+    });
+    // Update stats
+    db.query("UPDATE players SET player_name = ?, chat_title = ?, chat_type = ? WHERE player_id = ? AND chat_id = ?",
+        [name, msg.chat.title, msg.chat.type, msg.from.id, msg.chat.id], (err, r, f) => {
+        console.log("Failed to update user details. Using old details. Error: " + err.stack);
+    });
+    return true;
 };
--- server.js
+++ server.js
@@ -27,11 +27,13 @@ bot.onText(/\/gadmin_add (.+)/, (msg, match) => {
     let type = '';
     let oldstring = match[1];
     console.log('Splitting String ' + oldstring);
-    match = match[1].split(" ");
+    match = match[1].split("|||");
     // Not enough length
     if (match.length < 2) {
         bot.sendMessage(msg.chat.id, "Not enough arguments to register a new answer!\n" +
-            "You need to have a answer and a fake answer at least!\n\nFormat: /gadmin_add answer fake_answer [answer_type]");
+            "You need to have a answer and a fake answer at least!\n\nFormat: /gadmin_add answer|||fake_answer|||[answer_type]" +
+            "\n\nExample: If I want actual answer be Apple and fake answer be Orange under the Fruits type\n" +
+            "/gadmin_add Apple|||Orange|||Fruits");
         return;
     } else if (match.length === 2) type = defType;
     else type = match[2];
@@ -43,11 +45,16 @@ bot.onText(/\/gadmin_add (.+)/, (msg, match) => {
         case -1: message = "Unable to add category (" + type + "). Failed to add new answer"; break;
         case -2: message = "Unable to retrieve category (" + type + "). Failed to add new answer"; break;
         case -3: message = "Unable to add new answer (" + type + "). Failed to add new answer"; break;
-        case -4:
-        default: message = "An Unspecified DB Error occurred. Try again later"; break
+        default: message = "An Unspecified Error occurred. Try again later"; break
     }
     console.log("Executed command: /gadmin_add " + oldstring + ". Results: " + message);
     bot.sendMessage(msg.chat.id, message);
+});
+
+console.log('Registering any messages receiver');
+bot.on('message', (msg) => {
+    // Add user to DB
+    database.addUser(dbConnection, msg);
 });
 
 console.log('Finished initializing Telegram Bot!');
