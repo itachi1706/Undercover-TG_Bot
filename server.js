785695afe6e55ada6f269759530bb293afe68e94
itachi1706 <kennethsohyq@gmail.com>
2017-06-17 15:51:24 +0800

    Added a lot of TODO commands to do

--- server.js
+++ server.js
@@ -51,6 +51,63 @@ bot.onText(/\/gadmin_add (.+)/, (msg, match) => {
     bot.sendMessage(msg.chat.id, message);
 });
 
+// Matches "/create_game"
+console.log('Registering echo command');
+bot.onText(/\/create_game (.+)/, (msg, match) => {
+    // TODO: Create game (add record to DB) Defaulted to undercover
+    // TODO: Inline Reply Keyboard to select
+    // TODO: If there is a unstarted or inprogress game (state 0 or 1), do not create a new game
+    bot.sendMessage(msg.chat.id, "W.I.P Check back later!");
+});
+
+// Matches "/start [whatever]"
+console.log('Registering Start Game command');
+bot.onText(/\/start (.+)/, (msg, match) => {
+    // TODO: Starts the game (state 1)
+    // TODO: Randomly select a person to start and generate the sequence (probably by how they joined)
+    // TODO: If game has already started or abandoned (state 1 or 3) do nothing
+    // TODO: If game is completed (state 2), create a new game instance with the same settings and players
+    bot.sendMessage(msg.chat.id, "W.I.P Check back later!");
+});
+
+// Matches "/join"
+console.log('Registering Join Game command');
+bot.onText(/\/join (.+)/, (msg, match) => {
+    // TODO: Joins the game if its created but not started (state 0)
+    // TODO: Otherwise dont join game
+    bot.sendMessage(msg.chat.id, "W.I.P Check back later!");
+});
+
+// Matches "/abandon"
+console.log('Registering Abandon Game command');
+bot.onText(/\/abandon (.+)/, (msg, match) => {
+    // TODO: Abandons a started or unstarted game (state 0 or 1)
+    // TODO: Sets it to state 3
+    bot.sendMessage(msg.chat.id, "W.I.P Check back later!");
+});
+
+// Matches "/ans"
+console.log('Registering Give Answer command');
+bot.onText(/\/ans (.+)/, (msg, match) => {
+    // TODO: If its your turn, give an Answer and record it down in the turns table
+    // TODO: Sends a message of all messages for the game in the same turn
+    // TODO: At the end of the turn (everyone said something. Turns table match players table), can go accuse
+    // TODO: Only works when game is started (state 2) and not in accuse mode
+    bot.sendMessage(msg.chat.id, "W.I.P Check back later!");
+});
+
+// Matches "/accuse"
+console.log('Registering Accuse Player command');
+bot.onText(/\/abandon (.+)/, (msg, match) => {
+    // TODO: Only works when game is started (state 2)
+    // TODO: Only works in accuse mode (everyone given an answer and turn has yet to end
+    // TODO: Accuses a player (add/update accuse table) via username (need to see how to do it)
+    // TODO: When everyone accuses somebody already, kills the person
+    // TODO: If inncoent killed, continue game and increment turn, otherwise end game
+    // TODO: If there is an equal number of players being accused, the drawn players will write normally more answers and others can change their accusations
+    bot.sendMessage(msg.chat.id, "W.I.P Check back later!");
+});
+
 console.log('Registering any messages receiver');
 bot.on('message', (msg) => {
     // Add user to DB
